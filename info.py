dc5e0bfe790969e55211dc5150751e130533f3ea
Hemant Kumar <160343394+Speed10x@users.noreply.github.com>
2024-12-18 15:18:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -54,7 +54,7 @@ PREFIX = environ.get("PREFIX", "/")
 CHAT_GROUP = int(environ.get("CHAT_GROUP", " "))
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '.get('5223903292 6816205920').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5223903292 6816205920').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001986234690').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
